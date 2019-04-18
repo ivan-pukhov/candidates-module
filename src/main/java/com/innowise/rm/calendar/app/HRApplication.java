b5ffa0fc58e9fe815031ca6c0a3b4caa1408536f
morganco <30692607+morganco@users.noreply.github.com>
2019-04-18 14:23:43 +0300

    Creating database.

---
+++ src/main/java/com/innowise/rm/calendar/app/HRApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.rm.calendar.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HRApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HRApplication.class, args);
+    }
+}
